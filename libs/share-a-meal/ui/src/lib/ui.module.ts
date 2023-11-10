9f261a4bf471b22b07baaf1a1faf77ef3666f6eb
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-11-10 11:50:45 +0100

    added bootstrap

--- libs/share-a-meal/ui/src/lib/ui.module.ts
+++ libs/share-a-meal/ui/src/lib/ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
